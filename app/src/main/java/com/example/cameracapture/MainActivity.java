c7d20990fe9f59ac4c20548a6fa93f1a7f30593b
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-11-21 23:11:26 +0600

    intentCamera

--- app/src/main/java/com/example/cameracapture/MainActivity.java
+++ app/src/main/java/com/example/cameracapture/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.cameracapture;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imgCamera=findViewById(R.id.imgCamera);
+        Button btnCamera=findViewById(R.id.btnCamera);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iCamera=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
