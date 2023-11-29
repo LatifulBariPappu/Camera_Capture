d75667375d8c6bbe7bc3fb10f9b9090a43f8d1bb
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-11-29 19:23:35 +0600

    camera access completed

--- app/src/main/java/com/example/cameracapture/MainActivity.java
+++ app/src/main/java/com/example/cameracapture/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -12,17 +13,20 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private  final int CAMERA_REQ_CODE=100;
+    ImageView imgCamera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imgCamera=findViewById(R.id.imgCamera);
+        imgCamera=findViewById(R.id.imgCamera);
         Button btnCamera=findViewById(R.id.btnCamera);
 
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent iCamera=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(iCamera,CAMERA_REQ_CODE);
             }
         });
     }
@@ -30,5 +34,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==RESULT_OK){
+            if(requestCode==CAMERA_REQ_CODE){
+                //for camera
+                Bitmap img =(Bitmap)(data.getExtras().get("data"));
+                imgCamera.setImageBitmap(img);
+            }
+        }
     }
 }
